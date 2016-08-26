f744b6a075cf21c2c72e30e8f469aefdb6b2901d
Leslie Xin <leslie.xin@hotmail.com>
2016-08-26 10:18:30 -0400

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,13 +1,16 @@
 '''
 title           : app.py
 
-description     : The following code creates a Flask web server. This allows 
+description     : The following code creates a Flask web server which
+                  handles POST requests sent by the PHP form in the 
+                  first route ('/'). The second route ('/echo') then  
+                  commits the data to a MySQL database. 
 
-author          :
+author          : Leslie Xin 
 
 date            : August 2016
 
-python version  :2.7.12  
+python version  : 2.7.12  
 =============================================================================='''
 
 from flask import Flask, request, render_template
@@ -15,19 +18,24 @@ import MySQLdb
 from datetime import datetime
 import MySQLdb
 
+# Create Flask app
 app = Flask(__name__)
 
-# Connecting to database
+# Connecting to MySQL database
 db = MySQLdb.connect("localhost","root","lxin2016","test")
 cursor = db.cursor()
 
-# Finding the real time
+# Selecting table
+sql = "SELECT * FROM peapod"
+cursor.execute(sql)
+
+# Finding the real time using python's datetime library
 compile_hour = datetime.now().hour;
 compile_minute = datetime.now().minute;
 time = (compile_hour*100) + compile_minute
 print "Compile time: ", time
 
-# Form
+# Route returns a form which sends a POST request using user input
 @app.route("/")
 def hello():
 	return render_template('echoform3.php')
